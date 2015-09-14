2ebecdab6407d788882ad54d0903021a486eefbe
Joakim Ribier <joakim.ribier@gmail.com>
2015-09-14 22:48:26 +0200

    Missing 'scalaz' dependency

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,6 +9,7 @@ object Dependencies {
   val resolvers = Seq(
     "Typesafe Releases" at "http://repo.typesafe.com/typesafe/releases/", // Play things (outside Play project)
     "Pellucid Bintray" at "http://dl.bintray.com/pellucid/maven", // caseConfig
+    "Scalaz bintray" at "http://dl.bintray.com/scalaz/releases",
     Resolver.sonatypeRepo("releases"),
     Resolver.sonatypeRepo("snapshots")
   )
